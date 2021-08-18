894226008d830ebc8d4091b0aee6406b47ecc38f
kanoi-y <furafuratonbo@gmail.com>
2021-08-18 23:16:33 +0900

    無理やりカレンダーの初期化に成功

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import {
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const NOW = new Date();
-
 function Header(props) {
-  const [title, setTitle] = useState(
-    `${props.now.getFullYear()}/${props.now.getMonth() + 1}`
-  );
+  // const [title, setTitle] = useState(
+  //   `${props.now.getFullYear()}/${props.now.getMonth() + 1}`
+  // );
+  const title = `${props.now.getFullYear()}/${props.now.getMonth() + 1}`;
   const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const weekItem = week.map((day, i) => {
     return (
@@ -42,28 +41,53 @@ function Header(props) {
 function Body(props) {
   const [days, setDays] = useState([]);
 
-  // 月の最終日を求める関数
-  const getLastDay = (year, month) => {
-    return new Date(year, month, 0).getDate();
-  };
+  useEffect(() => {
+    // 月の最終日を求める関数
+    const getLastDay = (year, month) => {
+      return new Date(year, month, 0).getDate();
+    };
 
-  const getCalenderBody = (year, month) => {
-    // 先月の最終日
-    const beforeLastDay =
-      month === 0 ? getLastDay(year - 1, 12) : getLastDay(year, month);
+    const getCalenderBody = (year, month) => {
+      // 先月の最終日
+      const beforeLastDay =
+        month === 0 ? getLastDay(year - 1, 12) : getLastDay(year, month);
 
-    // 今月の最終日
-    const thisLastDay = getLastDay(year, month + 1);
+      // 今月の最終日
+      const thisLastDay = getLastDay(year, month + 1);
 
-    //  一日の曜日
-    const weekDay = new Date(year, month, 1).getDay();
+      //  一日の曜日
+      const weekDay = new Date(year, month, 1).getDay();
 
-    for (let i = 0; i < weekDay; i++) {
-      const num = beforeLastDay - weekDay + i + 1;
-      setDays([...days, { num, disable: true }]);
-    }
-  };
-  getCalenderBody();
+      // 今月の最終日の曜日
+      const endDayOfWeek = new Date(year, month + 1, 0).getDay();
+
+      let newDays = [];
+
+      for (let i = 0; i < weekDay; i++) {
+        const num = beforeLastDay - weekDay + i + 1;
+        //  setDays([...days, { num, disable: true }]);
+        newDays.push({ num, disable: true });
+      }
+
+      for (let i = 0; i < thisLastDay; i++) {
+        //  setDays([...days, { num: i + 1, disable: false }]);
+        newDays.push({ num: i + 1, disable: false });
+      }
+
+      for (let i = 0; i < 6 - endDayOfWeek; i++) {
+        //  setDays([...days, { num: i + 1, disable: true }]);
+        newDays.push({ num: i + 1, disable: true });
+      }
+
+      return newDays;
+    };
+
+    setDays(getCalenderBody(props.now.getFullYear(), props.now.getMonth() + 1));
+    console.log(props.now.getFullYear());
+    console.log(props.now.getMonth() + 1);
+    console.log(days);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   // const days = [
   //   { num: 1, disable: false },
@@ -126,7 +150,9 @@ function Footer() {
 }
 
 function Calender() {
+  const NOW = new Date();
   const [now, setNow] = useState(NOW);
+  console.log(now);
   return (
     <div className="calender">
       <Header now={now} setNow={setNow} />

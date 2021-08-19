9a10e3ff2a96b6d8e29b2f78ae27a42610eba12f
kanoi-y <furafuratonbo@gmail.com>
2021-08-19 15:38:25 +0900

    カレンダーの基本機能、完成

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const todayColor = {
+  backgroundColor: "rgb(255, 117, 75)",
+};
+
+const backColor = {
+  backgroundColor: "#fff",
+};
+
+const NOW = new Date();
+
 function Header(props) {
-  // const [title, setTitle] = useState(
-  //   `${props.now.getFullYear()}/${props.now.getMonth() + 1}`
-  // );
-  const title = `${props.now.getFullYear()}/${props.now.getMonth() + 1}`;
+  const { now, handleSetNow } = props;
+  const title = `${now.getFullYear()}/${now.getMonth() + 1}`;
   const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const weekItem = week.map((day, i) => {
     return (
@@ -27,11 +35,17 @@ function Header(props) {
       <FontAwesomeIcon
         className="calender_header_left calender_header_arrow"
         icon={faChevronLeft}
+        onClick={() =>
+          handleSetNow(new Date(now.getFullYear(), now.getMonth() - 1))
+        }
       />
       <h2 className="calender_header_title">{title}</h2>
       <FontAwesomeIcon
         className="calender_header_right calender_header_arrow"
         icon={faChevronRight}
+        onClick={() =>
+          handleSetNow(new Date(now.getFullYear(), now.getMonth() + 1))
+        }
       />
       {weekItem}
     </div>
@@ -39,6 +53,7 @@ function Header(props) {
 }
 
 function Body(props) {
+  const { now } = props;
   const [days, setDays] = useState([]);
 
   useEffect(() => {
@@ -65,67 +80,35 @@ function Body(props) {
 
       for (let i = 0; i < weekDay; i++) {
         const num = beforeLastDay - weekDay + i + 1;
-        //  setDays([...days, { num, disable: true }]);
-        newDays.push({ num, disable: true });
+        newDays.push({ num, disable: true, today: false });
       }
 
       for (let i = 0; i < thisLastDay; i++) {
-        //  setDays([...days, { num: i + 1, disable: false }]);
-        newDays.push({ num: i + 1, disable: false });
+        if (
+          now.getFullYear() === NOW.getFullYear() &&
+          now.getMonth() === NOW.getMonth() &&
+          i + 1 === NOW.getDate()
+        ) {
+          newDays.push({ num: i + 1, disable: false, today: true });
+        } else {
+          newDays.push({ num: i + 1, disable: false, today: false });
+        }
       }
 
       for (let i = 0; i < 6 - endDayOfWeek; i++) {
-        //  setDays([...days, { num: i + 1, disable: true }]);
-        newDays.push({ num: i + 1, disable: true });
+        newDays.push({ num: i + 1, disable: true, today: false });
       }
 
       return newDays;
     };
 
-    setDays(getCalenderBody(props.now.getFullYear(), props.now.getMonth() + 1));
-    console.log(props.now.getFullYear());
-    console.log(props.now.getMonth() + 1);
+    setDays(getCalenderBody(now.getFullYear(), now.getMonth()));
+    console.log(now.getFullYear());
+    console.log(now.getMonth() + 1);
     console.log(days);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [now]);
 
-  // const days = [
-  //   { num: 1, disable: false },
-  //   { num: 2, disable: false },
-  //   { num: 3, disable: false },
-  //   { num: 4, disable: false },
-  //   { num: 5, disable: false },
-  //   { num: 6, disable: false },
-  //   { num: 7, disable: false },
-  //   { num: 8, disable: false },
-  //   { num: 9, disable: false },
-  //   { num: 10, disable: false },
-  //   { num: 11, disable: false },
-  //   { num: 12, disable: false },
-  //   { num: 13, disable: false },
-  //   { num: 14, disable: false },
-  //   { num: 15, disable: false },
-  //   { num: 16, disable: false },
-  //   { num: 17, disable: false },
-  //   { num: 18, disable: false },
-  //   { num: 19, disable: false },
-  //   { num: 20, disable: false },
-  //   { num: 21, disable: false },
-  //   { num: 22, disable: false },
-  //   { num: 23, disable: false },
-  //   { num: 24, disable: false },
-  //   { num: 25, disable: false },
-  //   { num: 26, disable: false },
-  //   { num: 27, disable: false },
-  //   { num: 28, disable: false },
-  //   { num: 29, disable: false },
-  //   { num: 30, disable: false },
-  //   { num: 31, disable: false },
-  //   { num: 1, disable: true },
-  //   { num: 2, disable: true },
-  //   { num: 3, disable: true },
-  //   { num: 4, disable: true },
-  // ];
   const daysItems = days.map((day, i) => {
     return (
       <div
@@ -133,6 +116,7 @@ function Body(props) {
         className={
           day.disable ? "calender_body_day disabled" : "calender_body_day"
         }
+        style={day.today ? todayColor : backColor}
       >
         {day.num}
       </div>
@@ -141,23 +125,26 @@ function Body(props) {
   return <div className="calender_body">{daysItems}</div>;
 }
 
-function Footer() {
+function Footer(props) {
+  const { handleSetNow } = props;
   return (
     <div className="calender_footer">
-      <button>Today</button>
+      <button onClick={() => handleSetNow(NOW)}>Today</button>
     </div>
   );
 }
 
 function Calender() {
-  const NOW = new Date();
   const [now, setNow] = useState(NOW);
   console.log(now);
+  const handleSetNow = (newNow) => {
+    setNow(newNow);
+  };
   return (
     <div className="calender">
-      <Header now={now} setNow={setNow} />
-      <Body now={now} setNow={setNow} />
-      <Footer />
+      <Header now={now} handleSetNow={handleSetNow} />
+      <Body now={now} />
+      <Footer handleSetNow={handleSetNow} />
     </div>
   );
 }
